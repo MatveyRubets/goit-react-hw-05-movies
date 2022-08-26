8e04182f1cfdb8d526638300d54677c9360f64a3
MatveyRubets <rmatvey11@gmail.com>
2022-08-26 14:03:04 +0200

    lazy render

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,18 @@
-import { Suspense } from 'react';
+import { Suspense, lazy } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Navigation from 'components/Navigation/Navigation';
-import Home from 'pages/Home/Home';
-import Movies from 'pages/Movies/Movies';
-import MoviesDetails from 'pages/MoviesDetails/MoviesDetails';
-import Cast from 'components/Cast/Cast';
-import Reviews from 'components/Reviews/Reviews';
-import Loader from 'components/Loader/Loader';
+const Navigation = lazy(() => import('components/Navigation/Navigation'));
+const Home = lazy(() => import('pages/Home/Home'));
+const Movies = lazy(() => import('pages/Movies/Movies'));
+const MoviesDetails = lazy(() => import('pages/MoviesDetails/MoviesDetails'));
+const Cast = lazy(() => import('components/Cast/Cast'));
+const Reviews = lazy(() => import('components/Reviews/Reviews'));
+const Loader = lazy(() => import('components/Loader/Loader'));
 
 export const App = () => {
   return (
     <Router>
       <Navigation />
-      <Suspense element={Loader}>
+      <Suspense fallback={<Loader />}>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/movies" element={<Movies />} />
